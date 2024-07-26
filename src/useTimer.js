45b743b3687f73396afd998b4875bd2525766df1
Hien <jackiefablek21@gmail.com>
2024-07-26 18:25:46 +0700

    2nd

--- src/useTimer.js
+++ src/useTimer.js
@@ -1,4 +1,3 @@
-import { clear } from "@testing-library/user-event/dist/clear";
 import { useState, useRef } from "react";
 
 const useTimer = (ini = 0) => {
@@ -6,12 +5,12 @@ const useTimer = (ini = 0) => {
 
   const isStart = useRef(false);
   const active = useRef();
-  const refInterval = useRef(0);
+  let refInterval = useRef(0);
 
   const startTimer = () => {
     active.current.disabled = true;
     isStart.current = true;
-    refInterval = setInterval(() => {
+    refInterval.current = setInterval(() => {
       if (isStart.current) {
         setTime((time) => time + 1);
       }
@@ -19,13 +18,14 @@ const useTimer = (ini = 0) => {
   };
   const stopTimer = () => {
     isStart.current = false;
-    clearInterval(refInterval);
+    clearInterval(refInterval.current);
     active.current.disabled = false;
   };
   const resetTimer = () => {
     setTime(0);
-    clearInterval(refInterval);
+    clearInterval(refInterval.current);
     active.current.disabled = false;
+    isStart.current = false;
   };
 
   return { time, startTimer, stopTimer, resetTimer, active };
